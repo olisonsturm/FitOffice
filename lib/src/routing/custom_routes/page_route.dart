957f67ed5cac85d62ed355b701d418630da46857
Olison <olisonsturm@gmx.de>
2025-04-26 01:05:44 +0200

    feat: update on-boarding controller and page route methods to use explicit return types

--- lib/src/routing/custom_routes/page_route.dart
+++ lib/src/routing/custom_routes/page_route.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class TPageRoute{
 
-  static pageRoute(BuildContext context, int durationInMS,dynamic className ){
+  static void pageRoute(BuildContext context, int durationInMS,dynamic className ){
     Navigator.push(
       context,
       PageRouteBuilder(
