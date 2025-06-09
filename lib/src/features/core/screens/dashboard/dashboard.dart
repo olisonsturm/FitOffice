d75e1d0dc0e2e4459b5046a2938019ab63363b74
MaxSch-03 <max@maxschaefer.de>
2025-06-09 12:55:32 +0200

    Adding first tutorial steps on dashboard

--- lib/src/features/core/screens/dashboard/dashboard.dart
+++ lib/src/features/core/screens/dashboard/dashboard.dart
@@ -8,6 +8,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fit_office/src/features/core/screens/dashboard/widgets/appbar.dart';
 import 'package:fit_office/src/features/core/screens/dashboard/widgets/categories.dart';
 import 'package:fit_office/src/features/core/screens/profile/profile_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import '../../controllers/db_controller.dart';
 import '../../controllers/profile_controller.dart';
@@ -26,6 +29,11 @@ class DashboardState extends State<Dashboard> {
       GlobalKey<DashboardCategoriesState>();
   final ProfileController _profileController = Get.put(ProfileController());
   final DbController _dbController = DbController();
+  final GlobalKey _progressTabKey = GlobalKey();
+  final GlobalKey _libraryTabKey = GlobalKey();
+  final GlobalKey _statisticsTabKey = GlobalKey();
+  final GlobalKey _profileTabKey = GlobalKey();
+  List<TargetFocus> targets = [];
 
   int _selectedIndex = 0;
   String favoriteCount = '';
@@ -56,6 +64,163 @@ class DashboardState extends State<Dashboard> {
     });
   }
 
+  void checkAndStartTutorial() async {
+    final prefs = await SharedPreferences.getInstance();
+    final hasSeenTutorial = prefs.getBool('seenTutorial') ?? false;
+
+    if (!hasSeenTutorial) {
+      _initTargets();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        TutorialCoachMark(
+          targets: targets,
+          alignSkip: Alignment.bottomRight,
+          onFinish: () {
+            prefs.setBool('seenTutorial', true);
+          },
+          onClickTarget: (target) {},
+          onSkip: () {
+            prefs.setBool('seenTutorial', true);
+            return true;
+          },
+          onClickOverlay: (target) {},
+        ).show(context: context);
+      });
+    }
+  }
+
+  void _initTargets() {
+    targets.clear();
+    targets.addAll([
+      TargetFocus(
+        keyTarget: _progressTabKey,
+        enableOverlayTab: false,
+        shape: ShapeLightFocus.Circle,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                  width: 120,
+                  child: Lottie.asset('assets/lottie/FittyFuchsOffice.json'),
+                ),
+                Text(
+                  AppLocalizations.of(context)!.tProgressTutorial,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                _buildNavigationButtons(0),
+              ],
+            ),
+          ),
+        ],
+      ),
+      TargetFocus(
+        keyTarget: _libraryTabKey,
+        enableOverlayTab: false,
+        shape: ShapeLightFocus.Circle,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                  width: 120,
+                  child: Lottie.asset('assets/lottie/FittyFuchsOffice.json'),
+                ),
+                Text(
+                  AppLocalizations.of(context)!.tLibraryTutorial,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                _buildNavigationButtons(1),
+              ],
+            ),
+          ),
+        ],
+      ),
+      TargetFocus(
+        keyTarget: _progressTabKey,
+        enableOverlayTab: false,
+        shape: ShapeLightFocus.Circle,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                  width: 120,
+                  child: Lottie.asset('assets/lottie/FittyFuchsOffice.json'),
+                ),
+                Text(
+                  AppLocalizations.of(context)!.tStatisticsTutorial,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                _buildNavigationButtons(0),
+              ],
+            ),
+          ),
+        ],
+      ),
+      TargetFocus(
+        keyTarget: _progressTabKey,
+        enableOverlayTab: false,
+        shape: ShapeLightFocus.Circle,
+        contents: [
+          TargetContent(
+            align: ContentAlign.top,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                  width: 120,
+                  child: Lottie.asset('assets/lottie/FittyFuchsOffice.json'),
+                ),
+                Text(
+                  AppLocalizations.of(context)!.tProfileTutorial,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                _buildNavigationButtons(0),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ]);
+  }
+
+  late TutorialCoachMark tutorialCoachMark;
+
+  Widget _buildNavigationButtons(int step) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (step > 0)
+          TextButton(
+            onPressed: () {
+              tutorialCoachMark.previous();
+            },
+            child: Text(AppLocalizations.of(context)!.tBack),
+          ),
+        if (step < targets.length - 1)
+          TextButton(
+            onPressed: () {
+              tutorialCoachMark.next();
+            },
+            child: Text(AppLocalizations.of(context)!.tContinue),
+          ),
+        if (step == targets.length - 1)
+          ElevatedButton(
+            onPressed: () {
+              tutorialCoachMark.finish();
+            },
+            child: Text(AppLocalizations.of(context)!.tGotIt),
+          ),
+      ],
+    );
+  }
+
   final StreakController _streakController = Get.put(StreakController());
 
   @override
@@ -63,6 +228,9 @@ class DashboardState extends State<Dashboard> {
     super.initState();
     _streakController.loadStreakData();
     _loadUserFavorites();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      checkAndStartTutorial();
+    });
   }
 
 
@@ -114,8 +282,7 @@ class DashboardState extends State<Dashboard> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: (index) {
-          FocusScope.of(context)
-              .unfocus(); // <-- Fokus entfernen bei Navigation
+          FocusScope.of(context).unfocus();
           setState(() {
             _selectedIndex = index;
           });
@@ -125,19 +292,19 @@ class DashboardState extends State<Dashboard> {
         unselectedItemColor: tBottomNavBarUnselectedColor,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.route),
+            icon: Icon(Icons.route, key: _progressTabKey),
             label: AppLocalizations.of(context)!.tProgress,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.book),
+            icon: Icon(Icons.book, key: _libraryTabKey),
             label: AppLocalizations.of(context)!.tLibrary,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.insert_chart),
+            icon: Icon(Icons.insert_chart, key: _statisticsTabKey),
             label: AppLocalizations.of(context)!.tStatistics,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person, key: _profileTabKey),
             label: AppLocalizations.of(context)!.tProfile,
           ),
         ],
